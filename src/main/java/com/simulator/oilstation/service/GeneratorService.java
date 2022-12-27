6fb5468e769fd899421a571740f7095decb16f49
Roman Kuchereshko <romankuc@ext.inditex.com>
2022-12-27 18:35:09 +0200

    adjust according new lib

--- src/main/java/com/simulator/oilstation/service/GeneratorService.java
+++ src/main/java/com/simulator/oilstation/service/GeneratorService.java
@@ -14,7 +14,6 @@ import com.simulator.oilstation.domain.properties.ValueProperty;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oil.station.domain.frame.Frame;
-import oil.station.domain.value.Value;
 
 @Slf4j
 @Service
@@ -32,38 +31,19 @@ public class GeneratorService {
         final ValueProperty pressure = this.frameProperties.getPressure();
         final ValueProperty liquidFlowRate = this.frameProperties.getLiquidFlowRate();
 
-        return wellsUuids.stream().map(uuid -> Frame.builder()
+        return wellsUuids.stream()
+            .map(uuid -> Frame.builder()
                 .frameId(UUID.randomUUID())
                 .creationDateTime(LocalDateTime.now())
                 .wellId(uuid)
-                .voltage(Value.builder()
-                    .unitValue(this.getRandomValueInRange(voltage.getMinValue(), voltage.getMaxValue()))
-                    .unit(voltage.getUnit())
-                    .build())
-                .current(Value.builder()
-                    .unitValue(this.getRandomValueInRange(current.getMinValue(), current.getMaxValue()))
-                    .unit(current.getUnit())
-                    .build())
-                .speed(Value.builder()
-                    .unitValue(this.getRandomValueInRange(speed.getMinValue(), speed.getMaxValue()))
-                    .unit(speed.getUnit())
-                    .build())
-                .frequency(Value.builder()
-                    .unitValue(this.getRandomValueInRange(frequency.getMinValue(), frequency.getMaxValue()))
-                    .unit(frequency.getUnit())
-                    .build())
-                .temperature(Value.builder()
-                    .unitValue(this.getRandomValueInRange(temperature.getMinValue(), temperature.getMaxValue()))
-                    .unit(temperature.getUnit())
-                    .build())
-                .pressure(Value.builder()
-                    .unitValue(this.getRandomValueInRange(pressure.getMinValue(), pressure.getMaxValue()))
-                    .unit(pressure.getUnit())
-                    .build())
-                .liquidFlowRate(Value.builder()
-                    .unitValue(this.getRandomValueInRange(liquidFlowRate.getMinValue(), liquidFlowRate.getMaxValue()))
-                    .unit(liquidFlowRate.getUnit())
-                    .build()).build())
+                .voltage(this.getRandomValueInRange(voltage.getMinValue(), voltage.getMaxValue()))
+                .current(this.getRandomValueInRange(current.getMinValue(), current.getMaxValue()))
+                .speed(this.getRandomValueInRange(speed.getMinValue(), speed.getMaxValue()))
+                .frequency(this.getRandomValueInRange(frequency.getMinValue(), frequency.getMaxValue()))
+                .temperature(this.getRandomValueInRange(temperature.getMinValue(), temperature.getMaxValue()))
+                .pressure(this.getRandomValueInRange(pressure.getMinValue(), pressure.getMaxValue()))
+                .liquidFlowRate(this.getRandomValueInRange(liquidFlowRate.getMinValue(), liquidFlowRate.getMaxValue()))
+                .build())
             .toList();
     }
 
